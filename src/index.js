5c1e5ed8549caed9b87a9c238167352a25aa69e7
Ivan Chornyi <107806721+theIvanChornyi@users.noreply.github.com>
2022-11-15 18:12:33 +0200

    add requests

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import ReduxProvider from 'redux/store';
--- src/services/API/API.js
+++ src/services/API/API.js
@@ -10,3 +10,58 @@ export const token = {
     axios.defaults.headers.common['Authorization'] = '';
   },
 };
+
+// User registration
+export const registrationRequest = async user => {
+  return await axios.post('/auth/register', user);
+};
+
+// User authentication
+export const logInRequest = async user => {
+  return await axios.post('/auth/login', user);
+};
+
+// Logout;
+export const logOutRequest = async () => {
+  return await axios.post('/auth/logout');
+};
+
+// Get new pair of tokens (use Bearer {refreshToken} instead of accessToken)
+export const refreshtRequest = async token => {
+  return await axios.post('/auth/refresh', token);
+};
+
+// Get daily rate information based on your characteristics
+export const calculateDaylyRequest = async params => {
+  return await axios.post('/daily-rate', params);
+};
+
+// Post your characteristics to the website and get daily rate information
+export const calculateDaylyAuthRequest = async (id, params) => {
+  return await axios.post(`/daily-rate/${id}`, params);
+};
+
+// Search and get a list of products by query
+export const searchingProductRequest = async search => {
+  return await axios.get(`/daily-rate/?=${search}`);
+};
+
+// Post an eaten product
+export const addEatenProductRequest = async params => {
+  return await axios.post('/day', params);
+};
+
+// Delete eaten product
+export const deleteEatenProductRequest = async params => {
+  return await axios.delete('/day', params);
+};
+
+// Get info for day
+export const getInfoForDayRequest = async date => {
+  return await axios.post('/day/info', date);
+};
+
+// Get user info
+export const getUserInfoRequest = async date => {
+  return await axios.post('/user', date);
+};
