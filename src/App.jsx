8f82edf06720b112afc6ec2cbe03016689cdd529
Ivan Chornyi <107806721+theIvanChornyi@users.noreply.github.com>
2022-11-17 18:08:51 +0200

    fix pricate routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { lazy, Suspense, useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
 import Loader from 'components/Loader';
 import PrivateRout from 'components/PrivateRout';
 import { Background } from 'components/Background';
 
-// import { Test } from 'pages/Test/Test';
 import { useDispatch, useSelector } from 'react-redux';
 import { refreshUserThunk } from 'redux/auth/thunk.auth';
 import Header from 'components/Header';
@@ -31,15 +30,14 @@ export const App = () => {
         <Header />
         <Suspense fallback={<Loader />}>
           <Routes>
-            {/* <Route path="/test" element={<Test />} /> */}
-            <Route path="/home" element={<Home />} />
+            <Route path="/home" element={<Home />} end />
+            <Route path="/" element={<PrivateRout />}>
+              <Route path="diary" element={<Diary />} />
+              <Route path="calculator" element={<Calculator />} />
+            </Route>
             <Route path="/login" element={<Login />} />
             <Route path="/registration" element={<Registration />} />
-            <Route path="/" element={<PrivateRout />}>
-              <Route path="/diary" element={<Diary />} />
-              <Route path="/calculator" element={<Calculator />} />
-            </Route>
-            <Route path="*" element={<Home />} />
+            <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         </Suspense>
         <ToastContainer autoClose={5000} limit={1} />
