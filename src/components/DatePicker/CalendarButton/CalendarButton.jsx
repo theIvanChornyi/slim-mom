c19844f0f5745c6f32adbd98b69350627b3dd589
Ivan Chornyi <107806721+theIvanChornyi@users.noreply.github.com>
2022-11-18 11:55:27 +0200

    add close btn img

--- src/components/DatePicker/CalendarButton/CalendarButton.jsx
+++ src/components/DatePicker/CalendarButton/CalendarButton.jsx
@@ -1,23 +1,43 @@
 import { CalendarStyledBtn } from './CalendarButton.styled';
 
-export default function CalendarButton({ onClick }) {
+export default function CalendarButton({ onClick, showCalendar }) {
   return (
     <CalendarStyledBtn onClick={onClick} type="button">
-      <svg
-        width="20"
-        height="20"
-        viewBox="0 0 20 20"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path d="M15 9H13V11H15V9Z" fill="#9B9FAA" />
-        <path d="M11 9H9V11H11V9Z" fill="#9B9FAA" />
-        <path d="M7.00008 9H5.00006V11H7.00008V9Z" fill="#9B9FAA" />
-        <path
-          d="M17 2.00001H16V0H14V2.00001H6.00001V0H4V2.00001H3.00001C1.89001 2.00001 1.01003 2.90001 1.01003 4.00003L1 18C1 19.1 1.89001 20 3.00001 20H17C18.1 20 19 19.1 19 18V3.99998C19 2.90001 18.1 2.00001 17 2.00001ZM17 18H3.00001V6.99998H17V18Z"
-          fill="#9B9FAA"
-        />
-      </svg>
+      {!showCalendar ? (
+        <svg
+          width="20"
+          height="20"
+          viewBox="0 0 20 20"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M15 9H13V11H15V9Z" fill="#9B9FAA" />
+          <path d="M11 9H9V11H11V9Z" fill="#9B9FAA" />
+          <path d="M7.00008 9H5.00006V11H7.00008V9Z" fill="#9B9FAA" />
+          <path
+            d="M17 2.00001H16V0H14V2.00001H6.00001V0H4V2.00001H3.00001C1.89001 2.00001 1.01003 2.90001 1.01003 4.00003L1 18C1 19.1 1.89001 20 3.00001 20H17C18.1 20 19 19.1 19 18V3.99998C19 2.90001 18.1 2.00001 17 2.00001ZM17 18H3.00001V6.99998H17V18Z"
+            fill="#9B9FAA"
+          />
+        </svg>
+      ) : (
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g clipPath="url(#clip0_6_1348)">
+            <path d="M6 6L18 18" stroke="#212121" strokeWidth="2" />
+            <path d="M6 18L18 6" stroke="#212121" strokeWidth="2" />
+          </g>
+          <defs>
+            <clipPath id="clip0_6_1348">
+              <rect width="24" height="24" fill="white" />
+            </clipPath>
+          </defs>
+        </svg>
+      )}
     </CalendarStyledBtn>
   );
 }
--- src/components/DatePicker/DatePicker.jsx
+++ src/components/DatePicker/DatePicker.jsx
@@ -2,14 +2,25 @@ import CalendarButton from './CalendarButton/CalendarButton';
 import { DateContainer, DateString, MyCalendar } from './DatePicker.styled';
 import 'react-calendar/dist/Calendar.css';
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 export default function DatePicker({ date, setDate }) {
   const [showCalendar, setShowCalendar] = useState(false);
-  const normalizedDate = date.toLocaleDateString('en-GB').split('/').join('.');
+  const normalizedDate = date.toLocaleDateString('en-GB').replaceAll('/', '.');
 
   const handleToogleCalendar = () => {
     setShowCalendar(!showCalendar);
   };
+  const handleCloseCalendar = e => {
+    if (e.key === 'Escape') setShowCalendar(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleCloseCalendar);
+    return () => {
+      window.removeEventListener('keydown', handleCloseCalendar);
+    };
+  }, []);
 
   const handleCalendar = newDate => {
     setDate(new Date(newDate));
@@ -19,7 +30,10 @@ export default function DatePicker({ date, setDate }) {
   return (
     <DateContainer>
       <DateString>{normalizedDate}</DateString>
-      <CalendarButton onClick={handleToogleCalendar} />
+      <CalendarButton
+        showCalendar={showCalendar}
+        onClick={handleToogleCalendar}
+      />
       {showCalendar && <MyCalendar onClickDay={handleCalendar} value={date} />}
     </DateContainer>
   );
