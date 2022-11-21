060ec84c1a645a55ffb28561aea393c77758c5ea
Kate3587 <108617406+Kate3587@users.noreply.github.com>
2022-11-21 11:31:52 +0100

    filter added

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import { FilterInput, FilterWrap } from './Filter.styled';
+
+const Filter = ({ onChangeFilter, value}) =>(
+    <FilterWrap>
+        <FilterInput
+            onChange={onChangeFilter}
+            type="text"
+            value={value}
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces."
+            required
+            placeholder="Search product"
+        />
+    </FilterWrap>
+);
+    
+export default Filter;
+
+Filter.propTypes = {
+    onChangeFilter: PropTypes.func.isRequired,
+    value: PropTypes.string,
+};
---
+++ src/components/Filter/Filter.styled.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const FilterWrap = styled.div`
+    margin-right: auto;
+    margin-left: auto;
+    margin-bottom: 12px;
+    align-content: center;
+`;
+
+export const FilterInput = styled.input`
+    width: 250px;
+    padding: 3px;
+    font-size: 13px;
+    border-color: #264061;
+    outline: none;
+    border-radius: 5px;
+    margin-left: auto;
+    margin-right: auto;
+    background-color: #edf5ffb7;
+     @media (min-width: 768px) {
+    width: 300px;
+  }
+`;
