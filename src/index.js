a69404f7006f0e7408af93522b6b2df2ed3e4d4a
Ivan Chornyi <107806721+theIvanChornyi@users.noreply.github.com>
2022-11-22 11:33:49 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="Slim-Mom">
+    <BrowserRouter basename="slim-mom">
       <ReduxProvider>
         <App />
       </ReduxProvider>
